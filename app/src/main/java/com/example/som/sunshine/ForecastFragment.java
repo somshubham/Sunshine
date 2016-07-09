f3c849a7c870208320c1c50ef6bec969502f61ea
somshubham <somshubhams@gmail.com>
2016-07-09 13:56:20 +0530

    Added the http request

--- app/src/main/java/com/example/som/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/som/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.som.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
@@ -104,6 +105,10 @@ private ArrayAdapter<String> mForecastAdapter;
 
                 String forecast = mForecastAdapter.getItem(position);
                 Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                                           .putExtra(Intent.EXTRA_TEXT, forecast);
+                           startActivity(intent);
 
 
 
--- app/src/main/java/com/example/som/sunshine/MainActivity.java
+++ app/src/main/java/com/example/som/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.som.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -39,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            Intent intent=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
